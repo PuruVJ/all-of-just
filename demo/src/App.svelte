0b456bbda48da7739ce0f6589d7f3d68bd8f5512
Puru Vijay <awesomepuruvj@gmail.com>
2021-10-20 13:43:49 +0530

    0.0.6

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import logo from './assets/svelte.png';
   import Counter from './lib/Counter.svelte';
-  import { arraysShuffle } from 'all-of-just/arrays';
+  import {} from 'all-of-just';
 
   console.log(arraysShuffle([1, 2, 3, 4, 5, 6, 7]));
 </script>
